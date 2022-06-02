74ff05993cabbe39c30547277c8eb5ffe3b5a820
Paul Rivera <juniorrivera81@yahoo.com>
2022-06-01 21:53:28 -0600

    commit tree 8c2c84df3824e2a5ce58248d27f9bd966194480f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (){
+    newDOM()
+});
+function newDOM(){
+    const text = document.getElementById("text")
+    text.textContent = 'This is really cool!'
+
+}
+    
